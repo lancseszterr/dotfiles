2cb5cb9cedd9cdc6775f66d0d45b14012cf99373
lancseszterr <sebokgergo8@gmail.com>
2025-12-16 14:44:52 +0100

    Add cachyos repo

---
+++ install-scripts/repos.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# https://wiki.cachyos.org/features/optimized_repos/
+
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz
+cd cachyos-repo
+./cachyos-repo.sh
+
+cd ..
