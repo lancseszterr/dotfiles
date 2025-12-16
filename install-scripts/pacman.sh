10f6d8f5077a743b1bd597ef95d6d488600fb91f
lancseszterr <sebokgergo8@gmail.com>
2025-12-16 15:43:22 +0100

    Why am I this stupid T.T

--- install-scripts/pacman.sh
+++ install-scripts/pacman.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 # Replace #Color with Color
-sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
+sed -i 's/^#Color/Color/' /etc/pacman.conf
 
 # Add ILoveCandy after Color if not already in
-sudo grep -q "ILoveCandy" /etc/pacman.conf || sudo sed -i '/^Color/a ILoveCandy' /etc/pacman.conf
+grep -q "ILoveCandy" /etc/pacman.conf || sed -i '/^Color/a ILoveCandy' /etc/pacman.conf
 
 # Delete ParallelDownloads
-sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
+sed -i '/^ParallelDownloads/d' /etc/pacman.conf
 
 # Add ParallelDownloads
-sudo sed -i '/^Color/a ParallelDownloads = 7' /etc/pacman.conf
+sed -i '/^Color/a ParallelDownloads = 7' /etc/pacman.conf
