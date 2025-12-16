faa3a2bd3ce9b9b6f60494851383efc2c2525da9
lancseszterr <sebokgergo8@gmail.com>
2025-12-16 15:42:06 +0100

    Some changes

--- install-scripts/post-install.sh
+++ install-scripts/post-install.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+./pacman.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to change pacman config"
+fi
+
+./cachyos-repo.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to add Cachyos repositories"
+    echo "Exiting ..."
+    exit 2
+fi
+
 ./packages.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to install some packages"
+    echo "Exiting ..."
+fi
+
 ./mime-types.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to set mime-types"
+fi
+
+echo "Copying config files to ~/.config"
+cp -r ../.config ~/.config
