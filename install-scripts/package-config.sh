1f8695c8c5f33a058c9888f5b5badd169f47aff9
lancseszterr <sebokgergo8@gmail.com>
2025-12-17 12:06:22 +0100

    Some additions

--- install-scripts/package-config.sh
+++ install-scripts/package-config.sh
@@ -4,4 +4,21 @@ sudo systemctl enable ly@tty2.service
 sudo systemctl disable getty@tty2.service
 
 sudo ufw default deny incoming
-sudo ufw default deny routed
+sudo ufw default allow outgoing
+
+# Faster shutdown
+sudo mkdir -p /etc/systemd/system.conf.d
+
+cat <<EOF | sudo tee /etc/systemd/system.conf.d/10-faster-shutdown.conf
+[Manager]
+DefaultTimeoutStopSec=5s
+EOF
+sudo systemctl daemon-reload
+
+echo "Defaults passwd_tries=5" | sudo tee /etc/sudoers.d/passwd-tries
+sudo chmod 440 /etc/sudoers.d/passwd-tries
+
+sudo sed -i 's/^# *deny = .*/deny = 5/' /etc/security/faillock.conf
+
+sudo sed -i 's|^\(auth\s\+required\s\+pam_faillock.so\)\s\+preauth.*$|\1 preauth silent deny=5 unlock_time=120|' "/etc/pam.d/system-auth"
+sudo sed -i 's|^\(auth\s\+\[default=die\]\s\+pam_faillock.so\)\s\+authfail.*$|\1 authfail deny=5 unlock_time=120|' "/etc/pam.d/system-auth"
