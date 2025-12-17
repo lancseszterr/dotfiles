ed4ccaace8d2269ff8699e39878d5f0b2558e28a
lancseszterr <sebokgergo8@gmail.com>
2025-12-17 12:33:41 +0100

    Copy /etc and /boot first

--- post-install.sh
+++ post-install.sh
@@ -9,6 +9,13 @@ fi
 export DIR=$(pwd)
 export INSTALL_SCRIPTS_DIR=$DIR/install-scripts
 
+
+echo "Copying etc to /etc"
+sudo cp -r $DIR/etc /etc
+
+echo "Copying boot to /boot"
+sudo cp -r $DIR/boot /boot
+
 source "$INSTALL_SCRIPTS_DIR/pacman.sh"
 source "$INSTALL_SCRIPTS_DIR/cachyos-repo.sh"
 source "$INSTALL_SCRIPTS_DIR/packages.sh"
@@ -20,9 +27,3 @@ cp -r $DIR/.config ~/.config
 
 echo "Copying scripts files to ~/.scripts"
 cp -r $DIR/.scripts ~/.scripts
-
-echo "Copying etc to /etc"
-sudo cp -r $DIR/etc /etc
-
-echo "Copying boot to /boot"
-sudo cp -r $DIR/boot /boot
